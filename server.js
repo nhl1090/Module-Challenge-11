987099c6f4ebb0250bcf44cfa4a3eddf12b7498f
nhl1090 <neilhdz24@gmail.com>
2024-08-15 17:03:45 -0500

    installed dependencies and set them up in server.js]

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 3001
+// an npm package that can give each note a unique ID
+
+
