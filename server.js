5ef49a24ee182983ce969cd66833e4ab9e75b58e
nhl1090 <neilhdz24@gmail.com>
2024-08-15 17:07:57 -0500

    Used boilerplate middleware for parsing JSON and urlencoded form data

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ const app = express();
 const PORT = 3001
 // an npm package that can give each note a unique ID
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use(express.static('public'));
 
