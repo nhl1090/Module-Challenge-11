7d7ce009481e72308a43e916fc84534719b0f880
nhl1090 <neilhdz24@gmail.com>
2024-08-15 17:42:01 -0500

    Wrote the code for the port

--- server.js
+++ server.js
@@ -56,4 +56,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
   });
 
-  
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
