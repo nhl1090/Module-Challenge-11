c1d5f4ae4991cec25044e8addd0ad925d55c5db9
nhl1090 <neilhdz24@gmail.com>
2024-08-15 17:15:28 -0500

    Wrote code for reading notes

--- server.js
+++ server.js
@@ -11,3 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 app.use(express.static('public'));
 
+// API route to GET all notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).json({ error: 'Failed to read notes data' });
+      }
+      res.json(JSON.parse(data));
+    });
+  });
+
+// API route to SAVE a new note
+
+
+// API route to DELETE a note - BONUS
