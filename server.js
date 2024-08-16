2cd0d5acada660bd05a903f930e9726ad7438a71
nhl1090 <neilhdz24@gmail.com>
2024-08-16 09:39:31 -0500

    Fixed all server.js issues

--- server.js
+++ server.js
@@ -5,38 +5,28 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { v4: uuid } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// HTML route for notes.html
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
-});
-
-// HTML route for index.html
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
 
 // API route to GET all notes
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        return res.status(500).json({ error: 'Failed to read notes data' });
-      }
-      res.json(JSON.parse(data));
-    });
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to read notes data' });
+    }
+    res.json(JSON.parse(data));
   });
+});
 
 // API route to SAVE a new note
 
 app.post('/api/notes', (req, res) => {
-    const newNote = { id: uuid(), ...req.body };
+    const newNote = { id: uuidv4(), ...req.body };
     
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
@@ -54,6 +44,17 @@ app.post('/api/notes', (req, res) => {
       });
     });
   });
+
+// HTML route for notes.html
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+// HTML route for index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
 
   app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
